70743c67fed284d1cded2ac17b00241257d698ee
bensweet86 <bensweet@outlook.com.au>
2021-02-26 17:19:07 +1100

    Implemented robot snapshot state and JobHistoryMap

--- client/settings-cleaning-history-map.js
+++ client/settings-cleaning-history-map.js
@@ -5,10 +5,10 @@ import {ApiService} from "./services/api.service.js";
 async function updateSettingsCleaningHistoryMapPage() {
     const loadingBar = document.getElementById("loading-bar-settings-cleaning-history-map");
     let map = null;
-    
+
     loadingBar.setAttribute("indeterminate", "indeterminate");
     try {
-        let topPage = fn.getTopPage();
+        let topPage = window.fn.getTopPage();
         let mapName = await ApiService.getJobRecordMap(parseInt(topPage.data.recordId));
         let mapData = await ApiService.getJobRecordMapData(mapName);
         map = new VacuumMap(document.getElementById("settings-cleaning-history-map"));
